823d613b723651aab215b619926967c0539118b7
markhoward02 <mark@elexient.com>
2024-12-07 22:48:14 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+
+# # firebase login:ci
+
+# FIREBASE_TOKEN="1//05XGJn_edPFA2CgYIARAAGAUSNwF-L9IrdXYYM_YFL1AoiLrVLf7y4Hc5agHoaLQAESiYe1mQen6HiFcOkDGr60MozyQiS_X9CXw"
+
+# firebase deploy --token "$FIREBASE_TOKEN"
+
+hugo
